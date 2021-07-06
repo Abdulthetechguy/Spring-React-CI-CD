485d830b7f3927bc3425fcd6933fdf333cd6d6b3
Hakeem Abdulsalam <hakeemabdul2003@yahoo.com>
2021-07-06 08:12:03 -0500

    template update

---
+++ react-and-spring-data-rest/src/test/java/com/contoso/payroll/ReactAndSpringDataRestApplicationTests.java
@@ -0,0 +1,14 @@
+package com.contoso.payroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactAndSpringDataRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+
+}
